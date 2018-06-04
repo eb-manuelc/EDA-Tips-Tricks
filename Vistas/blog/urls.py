038e534a512633b530eba647c144cfeed6f31792
eb-manuelc <manuelc@eventbrite.com>
2018-06-04 11:40:50 -0300

    Vistas con clase terminado

--- Vistas/blog/urls.py
+++ Vistas/blog/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
-from . import views
+from blog.views import PostList, PostDetail, PostCreate, PostUpdate, PostDelete
 
 
 urlpatterns = [
-    url(r'^$', views.post_list),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^$', PostList.as_view(), name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$',
+        PostDetail.as_view(), name='post_detail'),
+    url(r'^post/delete/(?P<pk>[0-9]+)/$',
+        PostDelete.as_view(), name='post_delete'),
+    url(r'^post/new/$', PostCreate.as_view(), name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$',
+        PostUpdate.as_view(), name='post_edit'),
 ]
--- Vistas/blog/views.py
+++ Vistas/blog/views.py
@@ -1,47 +1,57 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.utils import timezone
-from django.shortcuts import render
 from blog.models import Post
-from django.shortcuts import get_object_or_404
-from .forms import PostForm
-from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
-def post_list(request):
-    posts = Post.objects.filter(
+class PostList(ListView):
+    model = Post
+    queryset = Post.objects.filter(
         published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+class PostDetail(DetailView):
+    model = Post
 
 
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title', 'text']
+    success_url = reverse_lazy('post_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(PostCreate, self).form_valid(form)
 
 
-def post_edit(request, pk):
-        post = get_object_or_404(Post, pk=pk)
-        if request.method == "POST":
-            form = PostForm(request.POST, instance=post)
-            if form.is_valid():
-                post = form.save(commit=False)
-                post.author = request.user
-                post.save()
-                return redirect('post_detail', pk=post.pk)
-        else:
-            form = PostForm(instance=post)
-        return render(request, 'blog/post_edit.html', {'form': form})
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['title', 'text']
+    success_url = reverse_lazy('post_list')
+
+
+class PostDelete(DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+
+# url('/contacts', MyContactsView.view())
+
+
+
+# class MyContactsView(ClassView):
+
+#     def get(self, *kwargs):
+#         pass
+
+#     def post(self, *kwargs):
+#         pass
+
+#     def delete(self):
+#         pass
+
+
+# def handle_request_test(self):
