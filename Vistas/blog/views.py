615e8599fca15487636e1a340bdff42a7ffe37fc
manuelc <manuelc@eventbrite.com>
2018-07-06 15:40:40 -0300

    fix Vistas con clases

--- Vistas/blog/views.py
+++ Vistas/blog/views.py
@@ -1,25 +1,30 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 from django.utils import timezone
 from blog.models import Post
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from django.urls import reverse_lazy
 import datetime
 
 
-class PostList(ListView):
+class PostMixin(object):
     model = Post
+    fields = ['title', 'text']
+
+
+class PostList(PostMixin, ListView):
     queryset = Post.objects.filter().order_by('published_date')
 
 
-class PostDetail(DetailView):
-    model = Post
+class PostDetail(PostMixin, DetailView):
+    pass
 
 
-class PostCreate(CreateView):
-    model = Post
-    fields = ['title', 'text']
+class PostCreate(PostMixin, CreateView):
     success_url = reverse_lazy('post_list')
 
     def form_valid(self, form):
@@ -28,30 +33,14 @@ class PostCreate(CreateView):
         return super(PostCreate, self).form_valid(form)
 
 
-class PostUpdate(UpdateView):
-    model = Post
-    fields = ['title', 'text']
+class PostUpdate(PostMixin, UpdateView):
     success_url = reverse_lazy('post_list')
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.published_date = datetime.datetime.now()
+        return super(PostUpdate, self).form_valid(form)
 
-class PostDelete(DeleteView):
-    model = Post
+
+class PostDelete(PostMixin, DeleteView):
     success_url = reverse_lazy('post_list')
-
-
-# url('/contacts', MyContactsView.view())
-
-
-# class MyContactsView(ClassView):
-
-#     def get(self, *kwargs):
-#         pass
-
-#     def post(self, *kwargs):
-#         pass
-
-#     def delete(self):
-#         pass
-
-
-# def handle_request_test(self):
